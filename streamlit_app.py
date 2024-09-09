551a874995d6132de512ad3b4cc0f60c91681fab
Heber Zapata <119647084+heber4543@users.noreply.github.com>
2024-09-09 00:07:14 +0000

    DISEÑO

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,24 +3,24 @@ import joblib
 import numpy as np
 from sklearn.datasets import load_iris
 
-# cargar modelo
+# cargar dataset
 iris = load_iris()
 
-# Cargar el modelo desde el archivo
+# cargar el modelo desde el archivo
 model = joblib.load('Model/iris_model.pkl')
 
-# Título de la aplicación
-st.title("Predicción de Flores Iris")
+# titulo aplicación
+st.title("Iris flower classifier with logistic regression")
 
-# Entradas de usuario
-sepal_length = st.number_input("Longitud del Sépalo (cm)", min_value=0.0)
-sepal_width = st.number_input("Ancho del Sépalo (cm)", min_value=0.0)
-petal_length = st.number_input("Longitud del Pétalo (cm)", min_value=0.0)
-petal_width = st.number_input("Ancho del Pétalo (cm)", min_value=0.0)
+# entradas de usuario
+sepal_length = st.number_input("Sepal length (cm)", min_value=0.0)
+sepal_width = st.number_input("Sepal width (cm)", min_value=0.0)
+petal_length = st.number_input("Petal length (cm)", min_value=0.0)
+petal_width = st.number_input("Petal width (cm)", min_value=0.0)
 
-# Realizar predicción al hacer clic en el botón
-if st.button("Predecir"):
+# predicción
+if st.button("Predict"):
     input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
     prediction = model.predict(input_data)
     pred_class = iris.target_names[prediction[0]]
-    st.write(f"La flor es probablemente: **{pred_class}**")
+    st.write(f"The flower is:: **{pred_class}**")
