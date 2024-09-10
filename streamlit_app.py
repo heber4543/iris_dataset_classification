dd07725fd2561d8de36fb4be02c080937719dd4c
Heber Zapata <119647084+heber4543@users.noreply.github.com>
2024-09-10 22:59:29 +0000

    interfaz

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,22 @@ model = joblib.load('Model/iris_model.pkl')
 # titulo aplicación
 st.title("Iris flower classifier with logistic regression")
 
+# indicaciones
+st.markdown("### This interface helps implement the linear regression model to predict the flower species. The model was trained with the Iris dataset. Whenever you are asked for information, you will be informed of the minimum and maximum values you can enter.")
+
 # entradas de usuario
+
+# sepal length
+st.markdown("#### Sepal length (cm). Minimum value: 4.30 / Maximum value: 7.90")
 sepal_length = st.number_input("Sepal length (cm)", min_value=0.0)
+# sepal width 
+st.markdown("#### Sepal width  (cm). Minimum value: 2.00 / Maximum value: 4.40")
 sepal_width = st.number_input("Sepal width (cm)", min_value=0.0)
+# petal length 
+st.markdown("#### Petal length  (cm). Minimum value: 1.00 / Maximum value: 6.90")
 petal_length = st.number_input("Petal length (cm)", min_value=0.0)
+# petal width 
+st.markdown("#### Petal width  (cm). Minimum value: 0.10 / Maximum value: 2.50")
 petal_width = st.number_input("Petal width (cm)", min_value=0.0)
 
 # predicción
